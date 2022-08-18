cd7d2249e4711519f098d6c721e0fec92b7fe0bc
ProbMoaz <nabelnadel21@gmail.com>
2022-08-18 20:09:00 +0200

    home page

---
+++ home.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../assets/css/style.css"/>
+    <link rel="icon" href="../assets/images/icon.png"/>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuezCanalUniversityHospital</title>
+</head>
+
+<body> 
+<header class="header">
+    <!-- <a href="#"> -->
+        <img class="logo" src="../assets/images/icon.png" alt="Logo">
+    <!-- </a> -->
+
+    <div class="nav_links">
+        <a href="#men">HOME</a>
+        <a href="#ex">LOG IN</a>
+        <a href="#">SIGN UP</a>
+        <a href="#">CONTACT US</a>
+    </div> 
+</header>
+
+<section class="home">     
+    <div class="homeLeft">    
+        <h1>ABOUT SUEZ CANAL UNIVERSITY HOSPITAL</h1>  <br>   
+        <p>
+            A state-of-the art modern facility in the heart of the ismailia government, it is spread over 10 acres and has a built-up area
+            of over 440000 square feet. Currently the hospital has 300+ beds with a capacity to expand to 400 beds.
+            Unity Hospital, Ahmedabad is a tertiary care flagship unit of the Unity Hospitals Group.The Hospital focuses on centres
+            of excellence like Cardiac Sciences, Neuro Sciences, Orthopaedics including Knee Replacement, Hip Replacement and Spine
+            Surgery, Cancer,
+            Emergency Medicine and Solid Organ Transplants besides the complete range of more than 35 allied medical disciplines
+            under the same roof.
+            Unity Hospital, Ahmedabad provides holistic healthcare that includes prevention, treatment, rehabilitation and health
+            education for patients, their families and clients by touching their lives.
+        </p>
+    </div>
+    <div class="homeRight">
+        <img src="/assets/images/hospital.jpg" alt="" >
+    </div>    
+</section>
+
+<section class="pricing">  
+    
+</section>
+
+
+    
+ 
+
+<section class="excut" id="ex">
+    <div class="card"><img src="../assets/images/eren.jpg" alt="eren"><p>moaz ahmed</p><p>Health Facilities Surveyor</p><div class="qoute">"Food is Food."</div></div>
+
+    <div class="card"><img src="../assets/images/joe.jpg" alt="joe"><p>youssef mohammed</p><p>Health Services Manager</p><div class="qoute">"The Best Food your pocket could handle."</div></div>
+
+    <div class="card"><img src="../assets/images/mo.jpg" alt="mo"><p>mohammed ibrahim</p><p>Healthcare Specialist</p><div class="qoute">"Honstley idk wut to write this far"</div></div>
+
+</section>
+
+
+</body>
+</html>
