7fc9395091769d5b7690ccd2f7347f5176a098b1
youss <youssef.mu.saber@gmail.com>
2022-08-19 16:25:05 +0200

    change the style of the registration page. And add the login page.

--- login.php
+++ login.php
@@ -1,1 +1,41 @@
-<a href="https://storyset.com/health">Health illustrations by Storyset</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="img"><span><img src="assets/images/Hospital patient.gif" alt=""></span></div>
+        <div class="right-side">
+            <div class="title">Login</div>
+            <div class="login-details">
+                <form method="post">
+                    <div class="user-details">
+                        <div class="input-box">
+                            <span class="details">Username</span>
+                            <input type="text" required>
+                        </div>
+                        
+                        <div class="input-box">
+                            <span class="details">Password</span>
+                            <input type="password" required>
+                        </div>
+
+                    <div class="btn">
+                        <input type="submit" value="Login">
+                    </div>
+                </form>
+
+                <div class="foot">
+                    <span class="txt">Don't have an account yet?</span>
+                    <span class="login"><a href="register.php">Signup</a></span>
+                </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
