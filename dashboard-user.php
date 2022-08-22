15001d4763cb37a6c8de34710a873b30247de509
ProbMoaz <nabelnadel21@gmail.com>
2022-08-22 21:13:54 +0200

    dashboard_user

---
+++ dashboard-user.php
@@ -0,0 +1,17 @@
+<?php
+include 'includes/login-process.php';
+session_start();echo"<center><br><h1 style='color: green;'>Welcome ".$_SESSION['globalUser']."</h1>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
